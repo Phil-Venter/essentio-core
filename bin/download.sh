22a5a4cc0635b8456a10cc6ebdd9efd5a843347f
Philip <mrphilventer@gmail.com>
2025-07-31 13:56:32 +0200

    chore(bin): simplify download script and add stubs

--- bin/download.sh
+++ bin/download.sh
@@ -2,52 +2,54 @@
 
 set -euo pipefail
 
-mkdir -p app
-mkdir -p public
-mkdir -p src/Api
-mkdir -p src/Cli
-mkdir -p src/Extra
-mkdir -p src/Http/Extra
-mkdir -p src/Web
+mkdir -p app public src/Api src/Cli src/Extra src/Http/Extra src/Web
+shared_prefix="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main"
 
 declare -A FILES=(
-    ["src/autoload.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/autoload.php"
+    ["src/autoload.php"]="${shared_prefix}/src/autoload.php"
 
     # base
-    ["src/Application.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Application.php"
-    ["src/Container.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Container.php"
-    ["src/Environment.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Environment.php"
-    ["src/FrameworkException.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/FrameworkException.php"
-    ["src/functions.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/functions.php"
+    ["src/Application.php"]="${shared_prefix}/src/Application.php"
+    ["src/Container.php"]="${shared_prefix}/src/Container.php"
+    ["src/Environment.php"]="${shared_prefix}/src/Environment.php"
+    ["src/FrameworkException.php"]="${shared_prefix}/src/FrameworkException.php"
+    ["src/functions.php"]="${shared_prefix}/src/functions.php"
 
     # http
-    ["src/Http/HttpException.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Http/HttpException.php"
-    ["src/Http/ValidationException.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Http/ValidationException.php"
-    ["src/Http/Request.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Http/Request.php"
-    ["src/Http/Response.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Http/Response.php"
-    ["src/Http/Route.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Http/Route.php"
-    ["src/Http/Router.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Http/Router.php"
-    ["src/Http/functions.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Http/functions.php"
+    ["src/Http/HttpException.php"]="${shared_prefix}/src/Http/HttpException.php"
+    ["src/Http/ValidationException.php"]="${shared_prefix}/src/Http/ValidationException.php"
+    ["src/Http/Request.php"]="${shared_prefix}/src/Http/Request.php"
+    ["src/Http/Response.php"]="${shared_prefix}/src/Http/Response.php"
+    ["src/Http/Route.php"]="${shared_prefix}/src/Http/Route.php"
+    ["src/Http/Router.php"]="${shared_prefix}/src/Http/Router.php"
+    ["src/Http/functions.php"]="${shared_prefix}/src/Http/functions.php"
 
     # api
-    ["src/Api/Jwt.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Api/Jwt.php"
-    ["src/Api/functions.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Api/functions.php"
+    ["src/Api/Jwt.php"]="${shared_prefix}/src/Api/Jwt.php"
+    ["src/Api/functions.php"]="${shared_prefix}/src/Api/functions.php"
 
     # web
-    ["src/Web/Session.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Web/Session.php"
-    ["src/Web/Template.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Web/Template.php"
-    ["src/Web/functions.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Web/functions.php"
+    ["src/Web/Session.php"]="${shared_prefix}/src/Web/Session.php"
+    ["src/Web/Template.php"]="${shared_prefix}/src/Web/Template.php"
+    ["src/Web/functions.php"]="${shared_prefix}/src/Web/functions.php"
 
     # cli
-    ["src/Cli/Argument.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Cli/Argument.php"
-    ["src/Cli/functions.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Cli/functions.php"
+    ["src/Cli/Argument.php"]="${shared_prefix}/src/Cli/Argument.php"
+    ["src/Cli/functions.php"]="${shared_prefix}/src/Cli/functions.php"
 
     # extras
-    ["src/Http/Extra/Cast.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Http/Extra/Cast.php"
-    ["src/Http/Extra/Validate.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Http/Extra/Validate.php"
-    ["src/Extra/HttpClient.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Extra/HttpClient.php"
-    ["src/Extra/Query.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Extra/Query.php"
-    ["src/Extra/functions.php"]="https://raw.githubusercontent.com/Phil-Venter/essentio-core/refs/heads/main/src/Extra/functions.php"
+    ["src/Http/Extra/Cast.php"]="${shared_prefix}/src/Http/Extra/Cast.php"
+    ["src/Http/Extra/Validate.php"]="${shared_prefix}/src/Http/Extra/Validate.php"
+    ["src/Extra/HttpClient.php"]="${shared_prefix}/src/Extra/HttpClient.php"
+    ["src/Extra/Query.php"]="${shared_prefix}/src/Extra/Query.php"
+    ["src/Extra/functions.php"]="${shared_prefix}/src/Extra/functions.php"
+
+    # stubs
+    ["app/PingController.php"]="${shared_prefix}/bin/stubs/app-ping_controller.stub"
+    ["routes.php"]="${shared_prefix}/bin/stubs/routes.stub"
+    ["public/index.php"]="${shared_prefix}/bin/stubs/public-index.stub"
+    ["cli"]="${shared_prefix}/bin/stubs/cli.stub"
+    ["bootstrap.php"]="${shared_prefix}/bin/stubs/bootstrap.stub"
 )
 
 command -v curl >/dev/null 2>&1 || { echo '❌ curl is required.'; exit 1; }
@@ -59,82 +61,4 @@ for path in "${!FILES[@]}"; do
 done
 wait
 
-echo "Creating: bootstrap.php"
-cat << 'EOF' > bootstrap.php
-<?php
-
-use Essentio\Extra\Query;
-
-once(PDO::class, fn() => new PDO("sqlite:" . base_path("database.sqlite")));
-bind(Query::class, fn() => new Query(app(PDO::class)));
-EOF
-
-echo "Creating: public/index.php"
-cat << 'EOF' > public/index.php
-<?php
-
-// autoload web
-(require_once __DIR__ . '/../src/autoload.php')([
-    'App'      => __DIR__ . '/../app',
-    'Essentio' => __DIR__ . '/../src',
-]);
-
-require_once __DIR__ . '/../src/functions.php';
-require_once __DIR__ . '/../src/Api/functions.php';
-require_once __DIR__ . '/../src/Extra/functions.php';
-require_once __DIR__ . '/../src/Http/functions.php';
-require_once __DIR__ . '/../src/Web/functions.php';
-
-// init and run web
-Essentio\Application::http(__DIR__ . '/..');
-require_once base_path('/bootstrap.php');
-require_once base_path('/routes.php');
-Essentio\Application::run();
-EOF
-
-echo "Creating: cli"
-cat << 'EOF' > cli
-#!/usr/bin/env php
-<?php
-
-// autoload cli
-(require_once __DIR__ . '/src/autoload.php')([
-    'Essentio' => __DIR__ . '/src'
-]);
-
-require_once __DIR__ . '/src/functions.php';
-require_once __DIR__ . '/src/Cli/functions.php';
-require_once __DIR__ . '/src/Extra/functions.php';
-
-// init and run cli
-Essentio\Application::cli(__DIR__);
-require_once base_path('/bootstrap.php');
-
-command('serve', fn() => exec('php -S localhost:8000 -t public'));
-
-Essentio\Application::run();
-EOF
-
-echo "Creating: routes.php"
-cat << 'EOF' > routes.php
-<?php
-
-get('__ping', [App\PingController::class, 'ping']);
-EOF
-
-echo "Creating: app/PingController.php"
-cat << 'EOF' > app/PingController.php
-<?php
-
-namespace App;
-
-use Essentio\Http\Response;
-
-class PingController
-{
-    public static function ping(): Response
-    {
-        return text('pong');
-    }
-}
-EOF
+chmod +x cli
